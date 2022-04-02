7dd1e0abcdca2ff1ab62415581f3b35687252f07
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2022-04-03 00:01:39 +0530

    [feat] added Homepage

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { NavBar } from "./NavBar/NavBar";
 export { Footer } from "./Footer/Footer";
 export { ToDoItem } from "./To-do/ToDoItem";
 export { Modal } from "./Modal/Modal";
+export { Hero } from "./Hero/Hero";
