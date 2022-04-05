4e6a5cd7e2f995ab8fd46cd85e4794acb0b9586a
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2022-04-05 23:59:10 +0530

    added 404 route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./styles/index.css";
 import { Routes, Route } from "react-router-dom";
 import { Dashboard, Pomodoro, Homepage } from "./pages";
+import { PageNotFound } from "./components";
 
 const App = () => {
   return (
@@ -8,7 +9,18 @@ const App = () => {
       <Route path="/" element={<Homepage />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/pomodoro" element={<Pomodoro />} />
-      <Route path="*" element={<h1>Page not found</h1>} />
+      <Route
+        path="*"
+        element={
+          <PageNotFound
+            errorMsg="Oops! Looks like you have lost your way. The page you're looking for
+does not exist.
+"
+            gotoMsg="Go to Homepage"
+            gotoPath="/"
+          />
+        }
+      />
     </Routes>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Footer } from "./Footer/Footer";
 export { ToDoItem } from "./To-do/ToDoItem";
 export { Modal } from "./Modal/Modal";
 export { Hero } from "./Hero/Hero";
+export { PageNotFound } from "./PageNotFound/PageNotFound";
